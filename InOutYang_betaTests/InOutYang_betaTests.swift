81b6285f72dc34cd64c7eff664837f486623a384
mark <mark77234@naver.com>
2024-11-30 17:01:12 +0900

    Initial Commit

---
+++ InOutYang_betaTests/InOutYang_betaTests.swift
@@ -0,0 +1,17 @@
+//
+//  InOutYang_betaTests.swift
+//  InOutYang_betaTests
+//
+//  Created by 이병찬 on 11/30/24.
+//
+
+import Testing
+@testable import InOutYang_beta
+
+struct InOutYang_betaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  INOUTY
+//
+//  Created by 이병찬 on 12/5/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
